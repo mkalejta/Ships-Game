035e6a7ecb4c935a9a45b9b6a4711c7120d69312
mkalejta <kalejtamikolaj@wp.pl>
2025-01-20 13:08:40 +0100

    Cleaning

--- server/routing/index.js
+++ server/routing/index.js
@@ -4,12 +4,10 @@ const game = require("./game")
 const registration = require('./registration')
 const login = require('./login')
 const logout = require('./logout')
-const rankingData = require('./rankingData')
 
 router.use("/game", game) // http://localhost:3000/api/game
 router.post("/registration", registration) // http://localhost:3000/api/registration
 router.post("/login", login) // http://localhost:3000/api/login
 router.post("/logout", logout) // http://localhost:3000/api/logout
-router.get("/rankingData", rankingData)
 
 module.exports = router
